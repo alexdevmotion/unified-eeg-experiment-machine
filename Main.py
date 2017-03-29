cfb9546d29125ba94de8d73c7708ce2433724f42
ahlexander_beast <alex@devmotion.ro>
2017-03-29 23:03:16 +0300

    Checkpoint: labels on one line and colored rectangles

--- Main.py
+++ Main.py
@@ -26,6 +26,7 @@ class FullScreenWindow:
         self.tk.bind("<Escape>", self.end_fullscreen)
         self.tk.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.tk.iconbitmap("misc/favicon.ico")
+        self.tk.minsize(width=600, height=400)
 
     def toggle_fullscreen(self, event=None):
         self.state = not self.state
@@ -67,11 +68,35 @@ class GUI:
 
         self.tk = tk
         self.threadedTasks = threadedTasks
+        self.keepCheckingWirelessStrength = False
+
+        Label(self.tk, text="Supported headsets: Emotiv Insight").pack(anchor=W)
+
+        donglePresentFrame = Frame()
+        donglePresentLabel = Label(donglePresentFrame, text="Dongle present: ")
+        donglePresentLabel.pack(side=LEFT)
+        self.donglePresentCanvas = Canvas(donglePresentFrame, width=60, height=30)
+        self.donglePresentCanvas.pack(side=RIGHT)
+        self.donglePresentRectangle = self.donglePresentCanvas.create_rectangle(0, 0, 100, 50, fill="red")
+        donglePresentFrame.pack(anchor=W)
+
+        headsetPresentFrame = Frame()
+        headsetPresentLabel = Label(headsetPresentFrame, text="Headset present: ")
+        headsetPresentLabel.pack(side=LEFT)
+        self.headsetPresentCanvas = Canvas(headsetPresentFrame, width=60, height=30)
+        self.headsetPresentCanvas.pack(side=LEFT)
+        self.headsetPresentRectangle = self.headsetPresentCanvas.create_rectangle(0, 0, 100, 50, fill="red")
+        headsetPresentFrame.pack(anchor=W)
+
+        wirelessStrengthFrame = Frame()
+        wirelessStrengthLabel = Label(wirelessStrengthFrame, text="Wireless strength: ")
+        wirelessStrengthLabel.pack(side=LEFT)
+        self.wirelessStrengthCanvas = Canvas(wirelessStrengthFrame, width=60, height=30)
+        self.wirelessStrengthCanvas.pack(side=LEFT)
+        self.wirelessStrengthRectangle = self.wirelessStrengthCanvas.create_rectangle(0, 0, 100, 50, fill="red")
+        wirelessStrengthFrame.pack(anchor=W)
 
         self.startThreads()
-
-        self.donglePresentLabel = StringVar()
-        Label(self.tk, textvariable=self.donglePresentLabel).pack()
 
         w.tk.mainloop()
 
@@ -81,11 +106,35 @@ class GUI:
     def updateDongleThread(self):
         resultQueue = Queue.Queue()
         self.threadedTasks.checkDonglePresent(resultQueue).join()
-        self.updateDonglePresent("No" if resultQueue.get() == 0 else "Yes")
-        self.tk.after(100, self.updateDongleThread)
+        result = resultQueue.get()
+        self.donglePresentCanvas.itemconfig(self.donglePresentRectangle, fill="red" if result == 0 else "green")
+        if result:
+            self.tk.after(100, self.updateHeadsetThread)
+    
+    def updateHeadsetThread(self):
+        resultQueue = Queue.Queue()
+        self.threadedTasks.checkHeadsetPresent(resultQueue).join()
+        result = resultQueue.get()
+        self.headsetPresentCanvas.itemconfig(self.headsetPresentRectangle, fill="red" if result == 0 else "green")
+        if result:
+            self.keepCheckingWirelessStrength = True
+            self.tk.after(100, self.updateWirelessThread)
 
-    def updateDonglePresent(self, status):
-        self.donglePresentLabel.set(status)
+    def updateWirelessThread(self):
+        resultQueue = Queue.Queue()
+        self.threadedTasks.getWirelessSignalStrength(resultQueue).join()
+        result = resultQueue.get()
+        if result == 0:
+            fill = "red"
+        elif result == 1:
+            fill = "yellow"
+        elif result == 2:
+            fill = "green"
+        else:
+            fill = "red"
+        self.wirelessStrengthCanvas.itemconfig(self.wirelessStrengthRectangle, fill=fill)
+        if self.keepCheckingWirelessStrength:
+            self.tk.after(100, self.updateWirelessThread)
 
 
 emotivHeadsetTasks = EmotivHeadsetThreadedTasks()
